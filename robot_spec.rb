ea61c3549a443384b3e30a678f57e376e1baef85
Alistair Roche <roche.a@gmail.com>
2013-09-25 15:41:55 +1000

    Change minitest output to show passing tests

--- robot_spec.rb
+++ robot_spec.rb
@@ -1,6 +1,8 @@
 require 'minitest/autorun'
-
+require "minitest/reporters"
 load 'robot.rb'
+
+MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
 
 describe Robot do
   it "can be instantiated" do
