4eadec027a4a622ae76858dc856e94d4c4427986
Alistair Roche <roche.a@gmail.com>
2013-09-25 15:54:35 +1000

    Add basic place method to Robot

--- robot_spec.rb
+++ robot_spec.rb
@@ -5,7 +5,17 @@ load 'robot.rb'
 MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
 
 describe Robot do
-  it "can be instantiated" do
+  before do
     @robot = Robot.new
+  end
+
+  describe "#place" do
+    it "sets the x, y and facing of the robot" do
+      @robot.place(1, 2, :north)
+
+      @robot.x.must_equal 1
+      @robot.y.must_equal 2
+      @robot.facing.must_equal :north
+    end
   end
 end
