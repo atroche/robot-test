6dcc72733c70d60e9d56d3d0bda3fa705627fbc2
Alistair Roche <roche.a@gmail.com>
2013-09-25 17:30:12 +1000

    Add script to pipe input to the Robot
    With test data file

---
+++ run_robot.rb
@@ -0,0 +1,23 @@
+load 'robot.rb'
+
+robot = Robot.new
+
+commands = File.read('test.data').split("\n")
+
+commands.each do |command|
+  instruction, args = command.split
+
+  case instruction
+  when "PLACE"
+    x, y, direction = args.split(',')
+    robot.place(x.to_i, y.to_i, direction.downcase.to_sym)
+  when "MOVE"
+    robot.move
+  when "LEFT"
+    robot.turn :left
+  when "RIGHT"
+    robot.turn :right
+  when "REPORT"
+    puts "#{robot.x},#{robot.y},#{robot.facing.upcase}"
+  end
+end
