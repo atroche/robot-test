e375eb0d785e6d6513af835de2e8405e575924f5
Alistair Roche <roche.a@gmail.com>
2013-09-25 16:02:07 +1000

    Make #place only work when given valid x and y

--- robot.rb
+++ robot.rb
@@ -2,8 +2,10 @@ class Robot
   attr_reader :x, :y, :facing
 
   def place(x, y, facing)
-    @x = x
-    @y = y
-    @facing = facing
+    if (x > 0 && x <= 5 && y >0 && y <= 5)
+      @x = x
+      @y = y
+      @facing = facing
+    end
   end
 end
--- robot_spec.rb
+++ robot_spec.rb
@@ -10,12 +10,29 @@ describe Robot do
   end
 
   describe "#place" do
-    it "sets the x, y and facing of the robot" do
-      @robot.place(1, 2, :north)
+    describe "when the specified position is on the table" do
+      it "sets the x, y and facing of the robot" do
+        @robot.place(1, 2, :north)
 
-      @robot.x.must_equal 1
-      @robot.y.must_equal 2
-      @robot.facing.must_equal :north
+        @robot.x.must_equal 1
+        @robot.y.must_equal 2
+        @robot.facing.must_equal :north
+      end
+    end
+
+    describe "when the specified position is not on the table" do
+      it "doesn't change the x, y and facing of the robot" do
+        @robot.place(1, 2, :north)
+
+        @robot.place(1, 6, :south)
+        @robot.place(-1, 3, :south)
+        @robot.place(6, 3, :south)
+        @robot.place(6, -1, :south)
+
+        @robot.x.must_equal 1
+        @robot.y.must_equal 2
+        @robot.facing.must_equal :north
+      end
     end
   end
 end
